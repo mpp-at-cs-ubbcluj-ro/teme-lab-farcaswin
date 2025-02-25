82b4d437c461dc6c12845e588fb63b8268771cd1
Farcas Andrei <andreifarcas21@gmail.com>
2025-02-25 12:35:56 +0200

    first

--- Lab1.java
+++ Lab1.java
@@ -1,5 +1,5 @@
 public class Lab1 {
     public static void main(String[] args){
-        System.out.println("Salut! Acesta este proiectul meu!!");
+        System.out.println("Salut! Acesta este u!!");
     }
 }
